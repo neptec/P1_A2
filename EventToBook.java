9cbd47131846dc692fd6ef19e80eb8c8727f54b6
Zeeshan Noorullah <zeeshan.noorullah@ey.com.au>
2023-04-18 15:56:40 +1000

    stage 1; EventToBook completed

--- EventToBook.java
+++ EventToBook.java
@@ -1,4 +1,52 @@
 public class EventToBook {
+    private String eventTitle;
+    private int pricePerPerson;
+    private int numOfPeople;
 
+    public EventToBook() {
+        this.eventTitle = "none";
+        this.pricePerPerson = 0;
+        this.numOfPeople = 0;
+    }
 
+    public EventToBook(String title, int price, int quantity) {
+        this.eventTitle = title;
+        this.pricePerPerson = price;
+        this.numOfPeople = quantity;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public int getPricePerPerson() {
+        return pricePerPerson;
+    }
+
+    public void setPricePerPerson(int pricePerPerson) {
+        this.pricePerPerson = pricePerPerson;
+    }
+
+    public int getNumOfPeople() {
+        return numOfPeople;
+    }
+
+    public void setNumOfPeople(int numOfPeople) {
+        this.numOfPeople = numOfPeople;
+    }
+
+    public int getCost() {
+        return pricePerPerson * numOfPeople;
+    }
+
+    @Override
+    public String toString() {
+        String paddedTitle = String.format("%-20s", eventTitle);
+        int totalCost = getCost();
+        return paddedTitle + ": " + numOfPeople + " @ $" + pricePerPerson + " = $" + totalCost;
+    }
 }
