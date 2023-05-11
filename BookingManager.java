6eab23e5456474312e32c55e48ecea20fc6f375c
Zeeshan Noorullah <zeeshan.noorullah@ey.com.au>
2023-05-11 12:32:43 +1000

    Stage 1 completed

--- BookingManager.java
+++ BookingManager.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 public class BookingManager {
     public static void stage1(EventToBook event){
+        Scanner scanner= new Scanner(System.in);
+        //prompting user for title of the event
+        System.out.print("Enter the title of the event: ");
+        String eventTitle = scanner.nextLine();
+        event.setEventTitle(eventTitle);
 
+        //prompting user for the price per person
+        System.out.print("Enter the price per person: ");
+        int pricePerPerson = scanner.nextInt();
+        event.setPricePerPerson(pricePerPerson);
+
+        //prompting user for the number of participants
+        System.out.print("Enter the number of participants: ");
+        int numOfPeople = scanner.nextInt();
+        event.setNumOfPeople(numOfPeople);
+
+        //calculating the final costs
+        int getCost = pricePerPerson*numOfPeople;
+        System.out.println(eventTitle + ": " + pricePerPerson + " @ $" + numOfPeople + " = $" + getCost);
     }
     public static void stage2(Booking booking){
 
